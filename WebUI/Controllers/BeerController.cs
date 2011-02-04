0924f3e09b55f82605d9c0c529f3578f03bac1ff
Tatham Oddie <tatham@oddie.com.au>
2011-02-04 16:45:07 +1100

    Fixed Slace's dodgy code formatting. Damn you OCD, damn you.

--- WebUI/Controllers/BeerController.cs
+++ WebUI/Controllers/BeerController.cs
@@ -8,43 +8,43 @@ namespace BeerTime.WebUI.Controllers
 {
     public class BeerController : Controller
     {
-        private static readonly Dictionary<double, IEnumerable<string>> Mappings
+        static readonly Dictionary<double, IEnumerable<string>> mappings
             = new Dictionary<double, IEnumerable<string>>
                   {
-                      { -12, new[]{ "the middle of the Pacific ocean" } },
-                      { -11, new[] { "Samoa" } },
-                      { -10, new[] { "Hawaii" } },
-                      { -9, new[] { "Alaska" } },
-                      { -8, new[] { "Los Angeles", "Hotel California" } },
-                      { -7, new[] { "Arizona" } },
-                      {-6, new[] {"Guadalajara"}},
-                      {-5, new[] {"Indiana"}},
-                      {-4.5, new[] {"Caracas"}},
-                      {-4, new[] { "Paraguay"}},
-                      {-3.5, new[] { "Newfoundland"}},
-                      {-3, new[] { "Buenos Aires"}},
-                      {-2, new[] { "the middle of the Atlantic ocean"}},
-                      {-1, new[] { "a random island off the coast of Senegal"}},
-                      {0, new[] { "London"}},
-                      {1, new[] { "Amsterdam"}},
-                      {2, new[] {"Helsinki"}},
-                      {3, new[] {"Moscow (or a Vodka)"}},
-                      {3.5, new[] {"Tehran"}},
-                      {4, new[] {"Abu Dhabi"}},
-                      {4.5, new[] {"Kabul, the dry state"}},
-                      {5, new[] {"Islamabad"}},
-                      {5.5, new[] {"Sri Jayawardenepura"}},
-                      {5.75, new[] {"the shadow of Mt Everest"}},
-                      {6, new[] {"Novosibirsk"}},
-                      {6.5, new[] {"Yangon "}},
-                      {7, new[] {"Bangkok"}},
-                      {8, new[] { "Yallabatharra, WA"}},
-                      {9, new[] {"Osaka"}},
-                      {9.5, new[] {"Uluṟu"}},
-                      { 10, new[] { "Sydney" } },
-                      {11, new[]{"New Caledonia"}},
-                      {12, new[] {"Fiji"}},
-                      {13, new[] {"Nuku'alofa"}}
+                      {-12, new[] { "the middle of the Pacific ocean" } },
+                      {-11, new[] { "Samoa" } },
+                      {-10, new[] { "Hawaii" } },
+                      {-9, new[] { "Alaska" } },
+                      {-8, new[] { "Los Angeles", "Hotel California" } },
+                      {-7, new[] { "Arizona" } },
+                      {-6, new[] { "Guadalajara"} },
+                      {-5, new[] { "Indiana"} },
+                      {-4.5, new[] { "Caracas"} },
+                      {-4, new[] { "Paraguay"} },
+                      {-3.5, new[] { "Newfoundland"} },
+                      {-3, new[] { "Buenos Aires"} },
+                      {-2, new[] { "the middle of the Atlantic ocean"} },
+                      {-1, new[] { "a random island off the coast of Senegal"} },
+                      {0, new[] { "London"} },
+                      {1, new[] { "Amsterdam"} },
+                      {2, new[] { "Helsinki"} },
+                      {3, new[] { "Moscow (or a Vodka)"} },
+                      {3.5, new[] { "Tehran"} },
+                      {4, new[] { "Abu Dhabi"} },
+                      {4.5, new[] { "Kabul, the dry state"} },
+                      {5, new[] { "Islamabad"} },
+                      {5.5, new[] { "Sri Jayawardenepura"} },
+                      {5.75, new[] { "the shadow of Mt Everest"} },
+                      {6, new[] { "Novosibirsk"} },
+                      {6.5, new[] { "Yangon "} },
+                      {7, new[] { "Bangkok"} },
+                      {8, new[] { "Yallabatharra, WA"} },
+                      {9, new[] { "Osaka"} },
+                      {9.5, new[] { "Uluṟu"} },
+                      {10, new[] { "Sydney" } },
+                      {11, new[] { "New Caledonia"}},
+                      {12, new[] { "Fiji"} },
+                      {13, new[] { "Nuku'alofa"} }
                   };
 
         [HttpGet]
@@ -55,10 +55,7 @@ namespace BeerTime.WebUI.Controllers
             var hours = result.GetUtcOffset(utcNow).Hours;
             var locationName = FindRandomLocation(hours, result.DisplayName);
 
-            ViewData.Model = new FindNoonViewModel
-                                 {
-                                     LocationName = locationName
-                                 };
+            ViewData.Model = new FindNoonViewModel { LocationName = locationName };
             return new DynamicActionResult();
         }
 
@@ -81,12 +78,7 @@ namespace BeerTime.WebUI.Controllers
 
         private static string FindRandomLocation(int hours, string defaultValue)
         {
-            if (!Mappings.ContainsKey(hours))
-            {
-                return defaultValue;
-            }
-
-            return Mappings[hours].First();
+            return mappings.ContainsKey(hours) ? mappings[hours].First() : defaultValue;
         }
     }
 }
